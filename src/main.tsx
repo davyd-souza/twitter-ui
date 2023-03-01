53911b7c1ed07b7a311ffa400634efafbaf11237
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-02-28 22:55:33 -0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+// DEPENDENCY
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// STYLE
+import './styles/global.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <div className='layout'>Twitter UI</div>
+  </React.StrictMode>
+)
