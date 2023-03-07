f50e5592b295654f583a0603ea7adecb02277314
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-03-06 22:36:10 -0300

    refactor: create style for default and remove from global

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -4,6 +4,9 @@ import { Outlet } from 'react-router-dom'
 // COMPONENT
 import { Sidebar } from '../../components/Sidebar'
 
+// STYLE
+import './Default.css'
+
 export function Default() {
   return (
     <div className='layout'>
